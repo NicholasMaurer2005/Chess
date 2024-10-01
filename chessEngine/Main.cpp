bc376012d2c4bf09850b848be753c55b88083f3c
Salockin <salockin9@gmail.com>
2024-10-01 16:25:18 -0400

    initial commit

---
+++ chessEngine/Main.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include "MoveGen.h"
+#include "BitBoard.h"
+#include "State.h"
+#include "Engine.h"
+#include <chrono>
+#include <string>
+#include <iostream>
+#include "ChessConstants.hpp"
+#include "ChessDisplay.h"
+
+int main()
+{
+	Engine engine;
+}
---
+++ chessEngine/temp.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+constexpr int RANK_MAX = 8;
+constexpr int FILE_MAX = 8;
+constexpr int MAX_BOARD_POSITIONS = FILE_MAX * RANK_MAX;
+constexpr int WHITE_COLOR = 0;
+constexpr int BLACK_COLOR = 1;
+constexpr int MAX_COLORS = 2;
